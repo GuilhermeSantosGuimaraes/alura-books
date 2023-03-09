a7c2209dbd1da5018a52a916f35734e06b0f7964
guimaraesgui <guimaraes11santos1@gmail.com>
2023-03-09 09:05:39 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import logo from './imagens/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
+    <div className='App'>
+      <header className='App-header'>
+        <div className='logo'>
+          <img src={logo} alt="logo do site"/>
+          <p><strong>Alura</strong> Books</p>
+        </div>
       </header>
     </div>
   );
